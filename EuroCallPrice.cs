51cfd674cca9de6b3386f71bed39766f36abe8af
jfbolanos <javierfbolanos@gmail.com>
2016-01-07 21:40:01 -0500

    This Program calculates the price of a European Call Option

---
+++ EuroCallPrice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuroCallOption
+{
+    class EuroCallPrice
+    {
+        static void Main(string[] args)
+        {
+            double[] UnderlyingPrice;
+            double[] CallPrice;
+            Random r = new Random();
+            
+            Console.WriteLine("Please Enter The Initial Asset Price:");
+            
+            Console.ReadLine();
+        }
+    }
+}
