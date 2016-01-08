e324b00eb447ea30136e66bed8610e0a70a4aca3
jfbolanos <javierfbolanos@gmail.com>
2016-01-07 21:59:05 -0500

    Inputs
    Reading inputs from user

--- EuroCallPrice.cs
+++ EuroCallPrice.cs
@@ -12,9 +12,16 @@ namespace EuroCallOption
         {
             double[] UnderlyingPrice;
             double[] CallPrice;
-            Random r = new Random();
+            Random r = new Random(); ///this is the class used to generate random variables
             
             Console.WriteLine("Please Enter The Initial Asset Price:");
+            double X0 = Console.ReadLine();
+            Console.WriteLine("Please Enter The Time To Expiration:");
+            double T = Console.ReadLine();
+            Console.WriteLine("Please Enter mu:");
+            double mu = Console.ReadLine();
+            Console.WriteLine("Please Enter sigma:");
+            double sigma = Console.ReadLine();
             
             Console.ReadLine();
         }
