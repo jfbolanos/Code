24103d46139932439adc8242f5a64d1378a59bbe
jfbolanos <javierfbolanos@gmail.com>
2016-01-08 21:24:16 -0500

    Track Stock Movement
    For last extra credit, now I've began to track the path of each
    simulated step. I duplicated the outputs to ensure that this change did
    not alter the output

--- EuroCallPriceEnhanced.cs
+++ EuroCallPriceEnhanced.cs
@@ -34,6 +34,7 @@ namespace EuroCallOption
         {
             double[] UnderlyingPrice;
             double[] CallPrice;
+            double[] CallPrice2;
             Random r = new Random(); ///this is the class used to generate random variables
             Discretization disc = new Discretization();
             
@@ -50,21 +51,28 @@ namespace EuroCallOption
             Console.WriteLine("Please enter The Number of Simulations to Run:");
             int N = Convert.ToInt32(Console.ReadLine());
             
-            double m = 400; ///Number of increments between t = 0 and t = T
+            int m = 400; ///Number of increments between t = 0 and t = T
             double delta = T / m; ///size of the increment
             CallPrice = new double[N]; ///now allocating the size of array based on the number of simulations
+            CallPrice2 = new double[N];
             double X;
             double U1;
             double U2;
             double Z;
             double sum1 = 0;
             double sum2 = 0;
+            double sum3 = 0;
+            double sum4 = 0;
             double OptionPrice;
+            double OptionPrice2;
             double StandardError;
+            double StandardError2;
+            UnderlyingPrice = new double[m+1];
 
             for (int i = 0; i < N; i++)
             {
                 X = X0;
+                UnderlyingPrice[0] = X0;
 
                 for (int j = 0; j < m; j++)
                 {
@@ -73,32 +81,41 @@ namespace EuroCallOption
                     U2 = r.NextDouble();
                     Z = Math.Sqrt(-2 * Math.Log(U1)) * Math.Cos(2 * Math.PI * U2);
                     X = disc.Euler(X, mu, sigma, delta, Z);
+                    UnderlyingPrice[j + 1] = disc.Euler(UnderlyingPrice[j], mu, sigma, delta, Z);
                     
 
                 }
 
                 CallPrice[i] = Math.Max(0, X - K);
+                CallPrice2[i] = Math.Max(0, UnderlyingPrice[m] - K);
 
             }
 
             for (int a = 0; a < N; a++ )
             {
                 sum1 = sum1 + CallPrice[a];
+                sum3 = sum3 + CallPrice2[a];
 
             }
 
             //Calculate the average which represents the option price
             OptionPrice = sum1 / N;
+            OptionPrice2 = sum3 / N;
 
             for (int b = 0; b < N; b++)
             {
                 sum2 = sum2 + Math.Pow((CallPrice[b] - OptionPrice), 2);
+                sum4 = sum4 + Math.Pow((CallPrice[b] - OptionPrice2), 2);
             }
 
             StandardError = Math.Sqrt(sum2 / (N - 1)) / Math.Sqrt(N);
+            StandardError2 = Math.Sqrt(sum4 / (N - 1)) / Math.Sqrt(N);
 
             Console.WriteLine("The Price of the Option is: " + OptionPrice);
             Console.WriteLine("The Standard Error of the Simulation is: " + StandardError);
+
+            Console.WriteLine("The Price of the Option is: " + OptionPrice2);
+            Console.WriteLine("The Standard Error of the Simulation is: " + StandardError2);
 
 
 
